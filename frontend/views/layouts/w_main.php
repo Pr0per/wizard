ac0290101f5858f54f6d89e0ca1cbefcf0007be9
YakovlevDV <iakovlev.08@mail.ru>
2016-08-12 15:37:09 +0500

    Add item top menu

--- frontend/views/layouts/w_main.php
+++ frontend/views/layouts/w_main.php
@@ -37,17 +37,18 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'На главную', 'url' => Yii::$app->homeUrl],
         ['label' => 'Каталог товаров', 'url' => ['/shop/']],
+        ['label' => 'Новости компании', 'url' => ['/news/']],
         ['label' => 'О нас', 'url' => ['/main/default/about']],
         ['label' => 'Контакты', 'url' => ['/main/default/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => 'Регистрация', 'url' => ['/main/default/signup']];
+        $menuItems[] = ['label' => 'Вход', 'url' => ['/main/default/login']];
     } else {
         $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
+            . Html::beginForm(['/main/default/logout'], 'post')
             . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
+                'Выход (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link']
             )
             . Html::endForm()
--- modules/main/controllers/DefaultController.php
+++ modules/main/controllers/DefaultController.php
@@ -4,6 +4,9 @@ namespace modules\main\controllers;
 
 use yii\web\Controller;
 use yii\web\ErrorAction;
+use Yii;
+use common\models\LoginForm;
+use modules\main\models\SignupForm;
 
 /**
  * Default controller for the `main` module
@@ -44,5 +47,59 @@ class DefaultController extends Controller
     public function actionContact()
     {
         return $this->render('contact');
+    }
+
+    /**
+     * Logs out the current user.
+     *
+     * @return mixed
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return mixed
+     */
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
     }
 }
